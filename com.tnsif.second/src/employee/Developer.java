750f7835121c53832f6d078018410b567fb84f00
Balu <Balu@DESKTOP-R136L04>
2025-09-21 21:24:16 +0530

    manasa

---
+++ com.tnsif.second/src/employee/Developer.java
@@ -0,0 +1,16 @@
+package employee;
+
+
+	public class Developer extends Employee {
+	    private String progLang;
+
+	    public Developer(String name, int employeeId, double salary, String progLang) {
+	        super(name, employeeId, salary);
+	        this.progLang = progLang;
+	    }
+
+	    public String getProgLang() { return progLang; }
+	    public void setProgLang(String progLang) { this.progLang = progLang; }
+	}
+
+
