4b9ba4b3dc07822d8021844a9430573a0ab0777b
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:55:14 +0530

    five

---
+++ com.tnsif.second/src/com/tnsif/five/phonemain.java
@@ -0,0 +1,13 @@
+package com.tnsif.five;
+
+public class  phonemain {
+	public static void main(String[] args) {
+        Jio j =new Jio();
+        Samsung s=new Samsung();
+        j.show();
+        s.show();
+}
+
+}
+
+
