cb25b59b6a41d08f527f894694300052e6082d3e
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:42:18 +0530

    five

---
+++ com.tnsif.second/src/com/tnsif/five/shape.java
@@ -0,0 +1,17 @@
+package com.tnsif.five;
+
+public class Shape {
+
+
+		protected float area;
+        abstract void Calrea();
+        public void show() {
+                System.out.println("this area of shape is : "+ area);
+        }
+
+
+	
+
+	}
+
+
