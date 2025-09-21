b6c3f97817f06a3e8ed1131a92f79ce578406864
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:46:42 +0530

    five

--- com.tnsif.second/src/com/tnsif/five/square.java
+++ com.tnsif.second/src/com/tnsif/five/square.java
@@ -1,5 +1,15 @@
 package com.tnsif.five;
 
 public class square {
+	private int side=2;
+    public Square(int side) {
+            this.side=side;
+    }
+    void CalArea() {
+            super.area=side*side;
+    }
 
 }
+	
+
+
