7c59d907b65322cb793c77e372cb88619b60d34b
Balu <Balu@DESKTOP-R136L04>
2025-09-21 19:58:55 +0530

    third

---
+++ com.tnsif.second/src/com/tnsif/thrid/checkprime.java
@@ -0,0 +1,20 @@
+package com.tnsif.thrid;
+
+public class checkprime {
+	public static void main(String[] args) {
+        int num = 19;  
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println(num + " is a Prime number");
+        } else {
+            System.out.println(num + " is Not a Prime number");
+        }
+    }
+}
+
+
---
+++ com.tnsif.second/src/com/tnsif/thrid/number.java
@@ -0,0 +1,14 @@
+package com.tnsif.thrid;
+
+public class number {
+	public static void main(String[] args) {
+        int a = 10; 
+        if (a % 2 == 0) {
+            System.out.println("number is even: " + a);
+        } else {
+            System.out.println("number is odd: " + a);
+        }
+    }
+}
+
+
