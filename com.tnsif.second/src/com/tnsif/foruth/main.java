a64b5bcac7877e46062464289fb67e307722afae
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:21:17 +0530

    fourth

---
+++ com.tnsif.second/src/com/tnsif/foruth/main.java
@@ -0,0 +1,18 @@
+package com.tnsif.foruth;
+
+public class main {
+	public static void main(String[] args) {
+        ConOsver s1 = new ConsOver();
+        ConsOver s2 = new ConsOver("Akhil");
+    ConsOver s3 = new ConsOver("Ravi", 21);
+    ConsOver s4 = new ConsOver("Sita", 22, "Computer Science");
+
+       s1.show();
+       s2.show();
+       s3.show();
+       s4.show();
+   }
+
+}
+
+
