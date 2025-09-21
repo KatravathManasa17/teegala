5cc0a1c8ce1cb20b7c06f8ef5d04900bca694e33
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:44:36 +0530

    five

---
+++ com.tnsif.second/src/com/tnsif/five/Rectangle.java
@@ -0,0 +1,16 @@
+package com.tnsif.five;
+
+public class Rectangle extends Shape{
+	private int length=5,breadth=2;
+    public Rectangle(int length,int breadth)
+    {
+            this.length=length;
+            this.breadth=breadth;
+    }
+    void CalArea() {
+            super.area=length*breadth;
+    }
+}
+	
+
+
