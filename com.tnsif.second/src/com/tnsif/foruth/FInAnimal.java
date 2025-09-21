ab9dc95db0034029739afc7149702b7b034c30c2
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:26:56 +0530

    fourth

---
+++ com.tnsif.second/src/com/tnsif/foruth/FInAnimal.java
@@ -0,0 +1,12 @@
+package com.tnsif.foruth;
+
+public class FInAnimal {
+	
+        final String type = "Lion";
+
+        final void sound() { 
+            System.out.println("Animals make sounds");
+        }
+    }
+
+
