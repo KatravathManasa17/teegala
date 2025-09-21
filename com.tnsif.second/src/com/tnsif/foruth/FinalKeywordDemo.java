9894dbe21bcc3678533fd5e12d8925e3d65c64da
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:30:49 +0530

    fourth

---
+++ com.tnsif.second/src/com/tnsif/foruth/FinalKeywordDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.foruth;
+
+public class FinalKeywordDemo {
+	public static void main(String[] args) {
+        FInAnimal d = new FInAnimal();
+        System.out.println("Animal: " + d.type);
+        d.sound();
+    }
+}
+
