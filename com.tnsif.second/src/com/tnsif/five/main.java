2311763d020becb3dd2ad2eb72e56a9de3511f8c
Balu <Balu@DESKTOP-R136L04>
2025-09-21 20:49:19 +0530

    five

---
+++ com.tnsif.second/src/com/tnsif/five/main.java
@@ -0,0 +1,15 @@
+package com.tnsif.five;
+
+public class main {
+	public static void main(String[] args) {
+        square sq = new square (9);
+        sq.CalArea();
+        sq.show();
+        Rectangle r=new Rectangle(8,9);
+        r.CalArea();
+        r.show();
+}
+
+}
+
+
